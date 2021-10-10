d3075539d35dbf6648ba5bc60989b52032e6dc58
dmitriy <yulin_dimon@mail.ru>
2021-10-10 23:29:27 +0300

    Connected the library socket.io

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const express = require('express');
 
 const app = express();
+
+const rooms = new Map([
+
+]);
+
+app.get('/users', (req,res)=> {
+    rooms.set('hello','')
+    // res.send('ответ')
+   res.json(rooms)
+});
+
+app.listen(9999, (err)=>{
+    if(err){
+        throw Error(err)
+    }
+    console.log('server start')
+});
