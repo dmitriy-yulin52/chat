4e67886826aaf4d95abb940b368cfb6d66e3469c
dmitriy <yulin_dimon@mail.ru>
2021-10-11 13:48:52 +0300

    Installed socket.io-client

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-const express = require('express');
+// const express = require('express');
+//
+// const app = express();
+// const server = require('http').Server(app);
+//
+//
+// // const options = {cors: { origin: "*" }}
+// const io = require('socket.io')(server);
+
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
 
-const rooms = new Map([
+const rooms = new Map();
 
-]);
-
-app.get('/users', (req,res)=> {
-    rooms.set('hello','')
-    // res.send('ответ')
-   res.json(rooms)
+app.get('/rooms', (req,res)=> {
+   res.json(rooms);
 });
 
-app.listen(9999, (err)=>{
+io.on('connection',socket => {
+    console.log('user connected',socket.id)
+})
+
+httpServer.listen(9000, (err)=>{
+    console.log('server start')
     if(err){
         throw Error(err)
     }
-    console.log('server start')
 });
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import io from 'socket.io-client'
+
+
+// const socket = io(`http://localhost:9999`)
+
 
 function App() {
+    const connectSocket = ()=> {
+        io(`http://localhost:9000`);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connectSocket}>connect</button>
     </div>
   );
 }
